0cf9569bce48a3bc259370c53a35c146f4c1c9af
samimeadad <sami.meadad@gmail.com>
2023-11-02 09:44:37 +0600

    floating number addition function added

--- hello-world.rs
+++ hello-world.rs
@@ -1,3 +1,12 @@
+fn add(a: f32, b: f32) -> f32 {
+    let sum = a + b;
+    return sum;
+}
+
 fn main() {
     println!("Hello, World!");
+
+    let sum = add(134.56, 243.22);
+
+    println!("{}", sum);
 }
